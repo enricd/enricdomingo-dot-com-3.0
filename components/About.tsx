22e0208a5edbb1a8b801282f4d00169725928b63
enricd <edomingodomenech@gmail.com>
2023-01-06 19:03:36 +0100

    starting about section and adding profile1 picture

---
+++ components/About.tsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+type Props = {}
+
+export default function About({}: Props) {
+  return (
+    <div className="flex flex-col relative h-screen text-center md:text-left max-w-7xl px-10 justify-evenly mx-auto items-center">
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+            About
+        </h3>
+
+        <motion.img 
+            src="" 
+        />
+    </div>
+  )
+}
